4df2b5991a4be41777f8951755f26d73f5085d96
basem909 <basemshams30@gmail.com>
2022-09-12 17:37:08 -0300

    Create Entities and groups and group_entities models

---
+++ app/models/entity.rb
@@ -0,0 +1,11 @@
+class Entity < ApplicationRecord
+
+  validates :name, presence: true
+  validates :author_id, presence: true
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0, alert: 'Amount must be greater than 0' }
+  has_many :group_entities, dependent: :destroy
+  has_many :groups, through: :group_entities
+  belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+
+end
