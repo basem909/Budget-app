902c239ce1c062d65bbdf5e304a1a7db1478f91d
basem909 <basemshams30@gmail.com>
2022-09-13 00:25:47 -0300

    Add a model test for user and group

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   subject { User.new(email: 'email@gmail.com', first_name: 'John', family_name: 'Jackson', password: '1234567890') }
 
   before { subject.save }
