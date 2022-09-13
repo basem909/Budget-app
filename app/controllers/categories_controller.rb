4980185cc92e719831fb7356c2f2b8cf7ecbddc9
basem909 <basemshams30@gmail.com>
2022-09-13 20:59:11 -0300

    Add Categories new and create controllers and index view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,4 +3,22 @@ class CategoriesController < ApplicationController
     @user = current_user
     @categories = Group.where(user_id: @user.id)
   end
+
+  def new
+    @category = Group.new
+    @user = current_user
+  end
+
+  def create
+    @category = Group.new(category_params)
+    @category.user_id = current_user.id
+    @category.save
+    redirect_to user_categories_path(current_user)
+  end
+
+  private
+
+  def category_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
