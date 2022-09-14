d86158563587f07ae7658be2fe978eea5bdff2e7
basem909 <basemshams30@gmail.com>
2022-09-14 00:23:38 -0300

    Create category new view

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,6 +4,6 @@ class Entity < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0, alert: 'Amount must be greater than 0' }
   has_many :group_entities, dependent: :destroy
-  has_many :groups, through: :groupentities
+  has_many :groups, through: :group_entities
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
 end
