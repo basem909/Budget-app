534d71292b839abc3229dd03383c061e487a78fe
basem909 <basemshams30@gmail.com>
2022-09-16 23:36:39 -0300

    Modify mailer action

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_mailer.default_url_options = {
-  host: 'https://budget-app-rails-mod.herokuapp.com'}
+  host: 'budget-app-rails-mod.herokuapp.com', :protocol => 'http'}
   config.action_mailer.asset_host = 'https://budget-app-rails-mod.herokuapp.com'
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
